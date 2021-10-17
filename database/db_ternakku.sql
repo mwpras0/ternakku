2f1b3c30bfdb15a1995e1db9f62f3253ee487ff4
Mohammad Wahyu Prasetyo <mwpras0@gmail.com>
2021-10-17 17:59:37 +0700

    Update Database

--- database/db_ternakku.sql
+++ database/db_ternakku.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 06, 2021 at 02:32 PM
+-- Generation Time: Oct 17, 2021 at 12:58 PM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 7.3.29
 
@@ -42,9 +42,21 @@ CREATE TABLE `beli` (
 
 CREATE TABLE `hewan` (
   `id_hewan` int(15) NOT NULL,
-  `jenis_hewan` char(4) NOT NULL,
-  `nama_hewan` char(4) NOT NULL
+  `jenis_hewan` int(15) NOT NULL COMMENT '1=sapi, 2=kambing, 3=domba, 4=kuda, 5=ayam, 6=bebek, 7=angsa, 8=merpati',
+  `id_kategori_produk` int(15) NOT NULL,
+  `nama_hewan` varchar(50) NOT NULL,
+  `harga_hewan` varchar(50) NOT NULL,
+  `detail_hewan` text NOT NULL,
+  `foto_hewan` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `hewan`
+--
+
+INSERT INTO `hewan` (`id_hewan`, `jenis_hewan`, `id_kategori_produk`, `nama_hewan`, `harga_hewan`, `detail_hewan`, `foto_hewan`) VALUES
+(4, 0, 4, 'Kambing Jawa', '2500000', 'Kambing siap kurban', 'gambar34.jpg'),
+(5, 0, 2, 'Sapi Perah', '25000000', 'Sapi perah dari indukan yang sehat terakreditasi, usia 3 bulan', 'gambar13.jpg');
 
 -- --------------------------------------------------------
 
@@ -61,6 +73,27 @@ CREATE TABLE `jual` (
   `keterangan` text NOT NULL,
   `stok` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `id_kategori_produk` int(15) NOT NULL,
+  `nama_kategori` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `kategori`
+--
+
+INSERT INTO `kategori` (`id_kategori_produk`, `nama_kategori`) VALUES
+(2, 'Sapi'),
+(3, 'Kuda'),
+(4, 'Kambing'),
+(5, 'Domba');
 
 -- --------------------------------------------------------
 
@@ -91,7 +124,7 @@ CREATE TABLE `user` (
   `password` varchar(255) NOT NULL,
   `alamat` varchar(50) NOT NULL,
   `telepon` varchar(15) NOT NULL,
-  `foto` text NOT NULL,
+  `foto_user` text NOT NULL,
   `posisi` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -99,8 +132,8 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id_user`, `nama`, `email`, `password`, `alamat`, `telepon`, `foto`, `posisi`) VALUES
-(1, 'Mohammad Wahyu Prasetyo', 'wahyu@gmail.com', 'e10adc3949ba59a', 'malang', '085645258965', '', 'Admin');
+INSERT INTO `user` (`id_user`, `nama`, `email`, `password`, `alamat`, `telepon`, `foto_user`, `posisi`) VALUES
+(16, 'Mohammad Wahyu Prasetyo', 'member@gmail.com', 'a8f5f167f44f4964e6c998dee827110c', 'asdads', '6549841', 'Logo.png', 'Penjual');
 
 --
 -- Indexes for dumped tables
@@ -126,6 +159,12 @@ ALTER TABLE `jual`
   ADD PRIMARY KEY (`id_jual`),
   ADD KEY `id_user` (`id_user`),
   ADD KEY `hewan` (`id_hewan`);
+
+--
+-- Indexes for table `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id_kategori_produk`);
 
 --
 -- Indexes for table `keranjang`
@@ -155,13 +194,19 @@ ALTER TABLE `beli`
 -- AUTO_INCREMENT for table `hewan`
 --
 ALTER TABLE `hewan`
-  MODIFY `id_hewan` int(15) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_hewan` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `jual`
 --
 ALTER TABLE `jual`
   MODIFY `id_jual` int(15) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `id_kategori_produk` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `keranjang`
@@ -173,7 +218,7 @@ ALTER TABLE `keranjang`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- Constraints for dumped tables
